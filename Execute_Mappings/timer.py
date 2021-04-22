d31335c41f75191aa520febc3ae2037f45c4461c
Salva5297 <salgonger1997@gmail.com>
2021-04-22 15:50:29 +0200

    Changed mappings rules

--- Execute_Mappings/timer.py
+++ Execute_Mappings/timer.py
@@ -3,7 +3,7 @@ import time
  
 start = time.time() #start time
 
-os.system('java -jar helio.jar --mappings=../Mappings --config=config.json --write=rdf.ttl --close --clear')
+os.system('java -jar helio.jar --mappings=../Mappings --config=config.json --write=obXML.ttl --close --clear')
  
 end = time.time()
 print("Elapsed time is  {}".format(end-start))
